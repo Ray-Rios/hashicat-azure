ec3a94cc7899a37ba9e4c90d2ee0dec1b7d97426
ray-rios <42rayrios@gmail.com>
2022-07-13 23:30:50 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/ray_stuff/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "network2"
+  resource_group_name = "${var.prefix}-workshop"
 }
